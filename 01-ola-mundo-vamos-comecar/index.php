f9f71e9e6d8304957c347935d9481702862264c2
Paulo Mariano <42322804+PauloHMariano@users.noreply.github.com>
2020-07-11 15:12:36 -0300

    Add files via upload

---
+++ 01-ola-mundo-vamos-comecar/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Iniciando um projeto</title>
+    <link rel="stylesheet" href="Assets/style.css"
+
+</head>
+<body>
+
+<?php
+
+/**
+ * Bloco PHP com variável start
+ * e comando de saída!
+ */
+$start = "Vamos Começar!";
+echo "<h1>Olá Mundo! {$start}</h1>";
+echo "<p id='js'>Loading...</p>";
+?>
+
+<script src="Assets/scripts.js"></script>
+</body>
+</html>
